c7827b9ed0d1fd12645ff1a5265051095ed4245d
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-08-29 12:23:43 +0200

    fix(vae): remove stale imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 from .decoder import Decoder
 from .decoder_convolutional import DecoderConvolutional
-from .decoder_discrete import DecoderDiscrete
 
 from .encoder import Encoder
 from .encoder_convolutional import EncoderConvolutional
-from .encoder_discrete import EncoderDiscrete

fix(vae): log mean NLL and KL

vae.py
@@ -81,7 +81,8 @@ def train(
     expected_log_likelihood = tf.reduce_sum(p_x_given_z.log_prob(x), axis=[1, 2, 3])
 
     elbo = tf.reduce_sum(expected_log_likelihood - beta * kl_divergence, axis=0)
-    tf.summary.scalar("negative log likelihood", expected_log_likelihood / batch_size)
+    tf.summary.scalar("kl_divergence", tf.reduce_mean(kl_divergence))
+    tf.summary.scalar("negative_log_likelihood", tf.reduce_mean(expected_log_likelihood))
 
     train_op = tf.train.AdamOptimizer().minimize(-elbo)
     summary_op = tf.summary.merge_all()
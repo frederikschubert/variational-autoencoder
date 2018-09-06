fix(vq_vae): remove moving average

models/vector_quantizer.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class VectorQuantizer:
     def __init__(self, num_codes, z_dimension):
         self.num_codes = num_codes
@@ -7,28 +8,20 @@ class VectorQuantizer:
         self.codebook = tf.get_variable(
             "codebook", shape=[num_codes, z_dimension], dtype=tf.float32
         )
-        self.ema = tf.train.ExponentialMovingAverage(decay=0.99)
-        self.train_op = self.ema.apply([self.codebook])
 
     def get_codebook_entries(self, one_hot_assignments):
         return tf.reduce_sum(
             tf.expand_dims(
                 one_hot_assignments, -1
             )  # [batch_size, latent_size, num_codes, 1]
-            * tf.reshape(
-                self.ema.average(self.codebook),
-                [1, 1, self.num_codes, self.z_dimension],
-            ),
+            * tf.reshape(self.codebook, [1, 1, self.num_codes, self.z_dimension]),
             axis=2,
         )
 
     def __call__(self, codes):
         distances = tf.norm(
             tf.expand_dims(codes, 2)  # [batch_size, latent_size, 1, z_dimension]
-            - tf.reshape(
-                self.ema.average(self.codebook),
-                [1, 1, self.num_codes, self.z_dimension],
-            ),
+            - tf.reshape(self.codebook, [1, 1, self.num_codes, self.z_dimension]),
             axis=-1,
         )
         code_assignments = tf.argmin(distances, -1)

vq_vae.py
@@ -55,12 +55,15 @@ def train(
     commitment_loss = tf.reduce_mean(
         tf.square(codes - tf.stop_gradient(nearest_codebook_entries))
     )
+    embedding_loss = tf.reduce_mean(
+        tf.square(tf.stop_gradient(nearest_codebook_entries) - codes)
+    )
 
     # Uniform prior over codes
     prior_distribution = tf.distributions.Multinomial(
         total_count=1.0, logits=tf.zeros([latent_size, num_codes])
     )
-    # This is constant during training
+
     prior_loss = -tf.reduce_mean(
         tf.reduce_sum(prior_distribution.log_prob(one_hot_code_assignments), 1)
     )
@@ -75,13 +78,14 @@ def train(
         prior_sample = prior_decoder_distribution.sample()
         tf.summary.image("prior_sample", tf.cast(prior_sample, tf.float32))
 
-    loss = reconstruction_loss + beta * commitment_loss + prior_loss
+    loss = reconstruction_loss + embedding_loss + beta * commitment_loss + prior_loss
 
     tf.summary.scalar("losses/total_loss", loss)
+    tf.summary.scalar("losses/embedding_loss", embedding_loss)
+    tf.summary.scalar("losses/prior_loss", prior_loss)
     tf.summary.scalar("losses/reconstruction_loss", reconstruction_loss)
     tf.summary.scalar("losses/commitment_loss", beta * commitment_loss)
 
-    # train_op = tf.group(quantizer.train_op, tf.train.AdamOptimizer().minimize(loss))
     train_op = tf.train.AdamOptimizer().minimize(loss)
     summary_op = tf.summary.merge_all()
 